394584d2b100c06c5cafb0633c243c47b7f3b6a9
Jaime Vargas <jaime.osorno@rocketfy.co>
2025-03-28 19:47:51 -0500

    feat(videos): implementar interceptor para carga de archivos > Incluye:   - LoggerInterceptor para seguimiento de operaciones   - Configuración de almacenamiento con multer   - Implementación de FileInterceptor   - Endpoint de carga de archivos

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -6,20 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { VideosService } from './videos.service';
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { LoggerInterceptor } from 'src/utils/logger.interceptor';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storage } from 'src/utils/media.handle';
 
 @Controller('videos')
 @ApiBearerAuth()
+@UseInterceptors(LoggerInterceptor)
 export class VideosController {
   constructor(private readonly videosService: VideosService) {}
 
   @Post()
   create(@Body() createVideoDto: CreateVideoDto) {
     return this.videosService.create(createVideoDto);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('avatar', { storage }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 
   @Get()
